aef7efdc32d372471caa58928eb2feae98f139b5
narendar124narendarguggilla124@gmail.com <narendar124narendarguggilla124@gmail.com>
2022-07-03 23:06:19 +0530

    Core java project...

---
+++ src/librarymanagement/User.java
@@ -0,0 +1,23 @@
+package librarymanagement;
+
+
+
+	public class User{
+		private int UserId;
+		private String UserName;
+		public int getUserId() {
+			return UserId;
+		}
+		public void setUserId(int userId) {
+			UserId = userId;
+		}
+		public String getUserName() {
+			return UserName;
+		}
+		public void setUserName(String userName) {
+			UserName = userName;
+		}
+		
+		
+
+			}
